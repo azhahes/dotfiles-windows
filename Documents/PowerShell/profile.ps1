fd133ba7b86c997ddb4885f0f061b48fb2e6dc7c
Azhaheswaran Subramaniyan <asubramaniya@lenovo.com>
2023-02-05 20:39:57 +0530

    add ideavimrc

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -4,13 +4,11 @@ oh-my-posh init pwsh --config $poshTheme | Invoke-Expression
 # PSFzf has undocumented option to use fd executable for
 # file and directory searching. This enables that option.
 Set-PsFzfOption -EnableFd:$true -EnableAliasFuzzyEdit `
-                                   -EnableAliasFuzzyZLocation `
-                                   -EnableAliasFuzzyGitStatus `
-                                   -EnableAliasFuzzyKillProcess `
-                                   -PSReadlineChordProvider 'Ctrl+t' `
-                                   -PSReadlineChordReverseHistory 'Ctrl+r'
-
-
+  -EnableAliasFuzzyZLocation `
+  -EnableAliasFuzzyGitStatus `
+  -EnableAliasFuzzyKillProcess `
+  -PSReadlineChordProvider 'Ctrl+t' `
+  -PSReadlineChordReverseHistory 'Ctrl+r'
 
 set-PSReadLineOption -PredictionViewStyle ListView
    
@@ -18,69 +16,85 @@ set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 
 Set-PSReadLineKeyHandler -Chord Ctrl+o -ScriptBlock {
-    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert('lfcd.ps1')
-    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-   }
+  [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+  [Microsoft.PowerShell.PSConsoleReadLine]::Insert('lfcd.ps1')
+  [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
 
 Set-PSReadLineKeyHandler -Chord Ctrl+i -ScriptBlock {
   Import-Module Terminal-Icons
   Import-Module posh-git
 }
 
+function Get-ChildItem-Hidden
+{
+  Get-ChildItem -Force
+}
 # Alias
-Set-Alias -Name fcd -Value Invoke-FuzzySetLocation 
+Set-Alias -Name fcd -Value Invoke-FuzzySetLocation
 Set-Alias -Name rg -Value Invoke-PsFzfRipgrep
-Set-Alias -Name v -Value nvim 
-Set-Alias -Name ls -Value Get-ChildItem -Force
+Set-Alias -Name v -Value nvim
+Set-Alias -Name lsh -Value Get-ChildItem-Hidden
 
 $env:PATH += ";C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client;C:\Program Files\AutoHotkey\v2"
 $env:SHELL = "pwsh"
 
-function keypress_wait {
-    param (
-        [int]$seconds = 10
-    )
-    $loops = $seconds*10
-    Write-Host "Connect to VPN? (y/n). After $seconds seconds vpn connection starts automatically.." -ForegroundColor Green
-    for ($i = 0; $i -le $loops; $i++){
-        if ([Console]::KeyAvailable) { break; }
-        Start-Sleep -Milliseconds 100
+function keypress_wait
+{
+  param (
+    [int]$seconds = 10
+  )
+  $loops = $seconds*10
+  Write-Host "Connect to VPN? (y/n). After $seconds seconds vpn connection starts automatically.." -ForegroundColor Green
+  for ($i = 0; $i -le $loops; $i++)
+  {
+    if ([Console]::KeyAvailable)
+    { break; 
     }
-    if ([Console]::KeyAvailable) { return [Console]::ReadKey($true); }
-    else { return $null ;}
+    Start-Sleep -Milliseconds 100
+  }
+  if ([Console]::KeyAvailable)
+  { return [Console]::ReadKey($true); 
+  } else
+  { return $null ;
+  }
 }
 
-function get-cred{
-    $PathToCredential = "$env:USERPROFILE\vpn-automation\encrypted-cred.txt"
-    if(!(Test-Path $PathToCredential)){
-      Write-Host "Enter username: " 
-      read-host | Add-Content $env:USERPROFILE\vpn-automation\encrypted-cred.txt
-      Write-Host "Enter password: "
-      read-host -assecurestring | convertfrom-securestring | Add-Content $env:USERPROFILE\vpn-automation\encrypted-cred.txt
-    }
-    $username = Get-Content $PathToCredential | Select -First 1 
-    $pwdTxt = Get-Content $PathToCredential | Select -Last 1 | ConvertTo-SecureString | ConvertFrom-SecureString -AsPlainText
-    return $username, $pwdTxt
+function get-cred
+{
+  $PathToCredential = "$env:USERPROFILE\vpn-automation\encrypted-cred.txt"
+  if(!(Test-Path $PathToCredential))
+  {
+    Write-Host "Enter username: " 
+    read-host | Add-Content $env:USERPROFILE\vpn-automation\encrypted-cred.txt
+    Write-Host "Enter password: "
+    read-host -assecurestring | convertfrom-securestring | Add-Content $env:USERPROFILE\vpn-automation\encrypted-cred.txt
+  }
+  $username = Get-Content $PathToCredential | Select-Object -First 1 
+  $pwdTxt = Get-Content $PathToCredential | Select-Object -Last 1 | ConvertTo-SecureString | ConvertFrom-SecureString -AsPlainText
+  return $username, $pwdTxt
 }
 
-function connect-vpn {
-    $username, $password = get-cred
-     autohotkey /script "$env:USERPROFILE\vpn-automation\lenovo-otp-automation.ahk" && autohotkey /script "$env:USERPROFILE\vpn-automation\copy-otp.ahk"
-     Start-Sleep -Seconds 4
-     $otp = Get-Clipboard
-     echo $username`n$password`n$otp | vpncli -s connect webvpnauto.lenovo.com
-     Start-Sleep -Seconds 3
-     vpnui
+function connect-vpn
+{
+  $username, $password = get-cred
+  autohotkey /script "$env:USERPROFILE\vpn-automation\lenovo-otp-automation.ahk" && autohotkey /script "$env:USERPROFILE\vpn-automation\copy-otp.ahk"
+  Start-Sleep -Seconds 4
+  $otp = Get-Clipboard
+  Write-Output $username`n$password`n$otp | vpncli -s connect webvpnauto.lenovo.com
+  Start-Sleep -Seconds 3
+  vpnui
 }
 
-if( vpncli state | Select-string "Disconnected" ){
-    Get-Process LenovoOTP64 -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue
-    Get-Process vpnui -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue
-    $result = keypress_wait
-    if($result -eq $null -or $result.keychar.ToString().ToLower() -eq 'y' ){
-        connect-vpn
-    }
-    Get-Process LenovoOTP64 -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue
+if( vpncli state | Select-string "Disconnected" )
+{
+  Get-Process LenovoOTP64 -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue
+  Get-Process vpnui -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue
+  $result = keypress_wait
+  if($null -eq $result -or $result.keychar.ToString().ToLower() -eq 'y' )
+  {
+    connect-vpn
+  }
+  Get-Process LenovoOTP64 -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue
 } 
 
